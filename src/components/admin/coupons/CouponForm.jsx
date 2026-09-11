9d1f7f81c36765afabf33b8b70c5d9a4adb08f4d
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-09-11 14:18:42 +0500

    cleaning up the extra code

--- src/components/admin/coupons/CouponForm.jsx
+++ src/components/admin/coupons/CouponForm.jsx
@@ -58,6 +58,7 @@ export default function CouponForm({ stores }) {
       let imageUrl = formData.image;
       let imageStoragePath = formData.imageStoragePath || null;
       let imagePublicId = formData.imagePublicId || null;
+
       if (imageFile) {
         setUploadingImage(true);
         const imageFormData = new FormData();
